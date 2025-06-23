a306ce24b1d82ce09a00936b6934198be8190b3a
ugnttad <dat200120040@gmail.com>
2025-06-23 10:28:15 +0700

    update lab4

--- Lab4/lab4/src/context/QuizContext.js
+++ Lab4/lab4/src/context/QuizContext.js
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useState } from 'react';
-import { initialQuizData } from '../data/quizData';
+import { initialQuizData } from '../data/QuizData.js';
 
 const QuizContext = createContext();
 
